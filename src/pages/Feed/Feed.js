c0c8947b7c1116a725917b98664b117f42fdddd5
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-11-29 17:03:05 -0600

    started socket.io-client

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-
+import openSocket from 'socket.io-client';
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
 import FeedEdit from '../../components/Feed/FeedEdit/FeedEdit';
